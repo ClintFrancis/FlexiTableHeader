f6b99e8f34dd417685e7437260d0654dc3c946f1
Clinton Francis <clinton.francis@xamarin.com>
2017-02-16 14:55:09 +0000

    Initial Commit

---
+++ FlexiTableApp/ViewController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using UIKit;
+
+namespace FlexiTableApp
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			headerView.BackgroundColor = UIColor.Orange;
+
+			string[] tableItems = new string[] { "Vegetables", "Fruits", "Flower Buds", "Legumes", "Bulbs", "Tubers" };
+			tableView.Source = new TableSource(tableItems);
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+
+	public class TableSource : UITableViewSource
+	{
+
+		string[] TableItems;
+		string CellIdentifier = "TableCell";
+
+		public TableSource(string[] items)
+		{
+			TableItems = items;
+		}
+
+		public override nint RowsInSection(UITableView tableview, nint section)
+		{
+			return TableItems.Length;
+		}
+
+		public override UITableViewCell GetCell(UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			UITableViewCell cell = tableView.DequeueReusableCell(CellIdentifier);
+			string item = TableItems[indexPath.Row];
+
+			//---- if there are no cells to reuse, create a new one
+			if (cell == null)
+			{ cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier); }
+
+			cell.TextLabel.Text = item;
+
+			return cell;
+		}
+	}
+}
---
+++ FlexiTableApp/ViewController.designer.cs
@@ -0,0 +1,42 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace FlexiTableApp
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.NSLayoutConstraint headerHeightConstraint { get; set; }
+
+		[Outlet]
+		UIKit.UIView headerView { get; set; }
+
+		[Outlet]
+		UIKit.UITableView tableView { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (headerView != null) {
+				headerView.Dispose ();
+				headerView = null;
+			}
+
+			if (tableView != null) {
+				tableView.Dispose ();
+				tableView = null;
+			}
+
+			if (headerHeightConstraint != null) {
+				headerHeightConstraint.Dispose ();
+				headerHeightConstraint = null;
+			}
+		}
+	}
+}
