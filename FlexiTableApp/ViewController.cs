743ade03a202ad359dc6e4ec6ecbace9dc90f525
Clinton Francis <clinton.francis@xamarin.com>
2017-02-16 15:46:48 +0000

    Basic scaling

--- FlexiTableApp/ViewController.cs
+++ FlexiTableApp/ViewController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using CoreAnimation;
 using UIKit;
 
 namespace FlexiTableApp
@@ -17,9 +17,11 @@ namespace FlexiTableApp
 			// Perform any additional setup after loading the view, typically from a nib.
 
 			headerView.BackgroundColor = UIColor.Orange;
+			headerView.Layer.ZPosition = 1;
+			//headerView.Layer.AnchorPoint = new CoreGraphics.CGPoint(0.5, 0);
 
 			string[] tableItems = new string[] { "Vegetables", "Fruits", "Flower Buds", "Legumes", "Bulbs", "Tubers" };
-			tableView.Source = new TableSource(tableItems);
+			tableView.Source = new TableSource(tableItems, headerHeightConstraint, headerView);
 		}
 
 		public override void DidReceiveMemoryWarning()
@@ -34,10 +36,16 @@ namespace FlexiTableApp
 
 		string[] TableItems;
 		string CellIdentifier = "TableCell";
+		NSLayoutConstraint headerHeight;
+		nfloat baseHeight;
+		UIView header;
 
-		public TableSource(string[] items)
+		public TableSource(string[] items, NSLayoutConstraint headerHeight, UIView header)
 		{
 			TableItems = items;
+			this.headerHeight = headerHeight;
+			baseHeight = headerHeight.Constant;
+			this.header = header;
 		}
 
 		public override nint RowsInSection(UITableView tableview, nint section)
@@ -58,5 +66,38 @@ namespace FlexiTableApp
 
 			return cell;
 		}
+
+		public override void Scrolled(UIScrollView scrollView)
+		{
+			var offset = scrollView.ContentOffset.Y;
+			var headerTransform = CATransform3D.Identity;
+
+			//var height = baseHeight - offset;
+			//if (height < 60f)
+			//	height = 60f;
+
+			// Pull Down
+			if (offset < 0)
+			{
+				nfloat headerScaleFactor = -(offset) / header.Bounds.Height;
+				nfloat headerSizevariation = ((header.Bounds.Height * (1.0f + headerScaleFactor)) - header.Bounds.Height) / 2.0f;
+
+				headerTransform = headerTransform.Translate(0f, headerSizevariation, 0f);
+				headerTransform = headerTransform.Scale(1.0f + headerScaleFactor, 1.0f + headerScaleFactor, 0f);
+
+				header.Layer.Transform = headerTransform;
+			}
+
+			// Scroll up / down
+			else
+			{
+				// Header
+				headerTransform = headerTransform.Translate(0, (nfloat)Math.Max(-60f, -offset), 0);
+				header.Layer.Transform = headerTransform;
+				headerHeight.Constant = header.Layer.Frame.Height;
+
+				Console.WriteLine("Layer bounds {0}", header.Layer.Bounds);
+			}
+ 		}
 	}
 }
